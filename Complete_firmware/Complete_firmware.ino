2b22ffb421017a8f442d93de40ba3ea8ee6be233
hps-annah <asingh@hps-program.com>
2022-07-26 17:33:31 -0400

    Update Complete_firmware.ino

--- Complete_firmware/Complete_firmware.ino
+++ Complete_firmware/Complete_firmware.ino
@@ -28,6 +28,7 @@ int upState = 0;
 int setState = 0;
 int alarmState = 0; 
 int buttonPresses = 0;
+int buzzer = A1;
 
 //RTC
 int varMin = 0;
@@ -38,6 +39,11 @@ int varmm = 0;
 int varyy = 0;
 int hours = 0;
 int Minutes = 0;
+int time_min = 0;
+int time_hours = 0;
+
+int alarmMin = 0;
+int alarmHr = 0;
 
 // for the 16x2 LCD
 #define rs A0 
@@ -142,23 +148,25 @@ void loop () {
     delay(100); 
 }
   void Buttons();
+  //if alarm time = rtc time set alarm off
+  void Alarm();
 }
 
 #define countof(a) (sizeof(a) / sizeof(a[0]))
 
 //check buttons
 void Buttons()
-{
+{ //if alarm off button == LOW && alarm is on --> turn alarm off
   downState = digitalRead(downButton);
   upState = digitalRead(upButton);
   setState = digitalRead(downButton);
   alarmState = digitalRead(alarmButton);
-  
+
   //check how many times set was pressed 
   if (setState == LOW) {
     delay(200);
     Serial.println("Button pressed once");
-
+    
     if (buttonPresses = 0){
       buttonTime = millis();
       timeLimit = buttonTime + 500;
@@ -179,11 +187,13 @@ void Buttons()
       Serial.println("Button pressed once");
     }
     }
+  }
  if (buttonPresses == 1){
   void changeTime();
  }
  else if (buttonPresses == 2){
   void setAlarm();
+}
 }
     
  //set everything back to 0
@@ -193,16 +203,15 @@ void Buttons()
 }
 
 void changeTime(){
-  
-}
-
-
-
-//print RTC time on console, LCD, and 7-seg
-void printDateTime(const RtcDateTime& dt)
-{ 
+  // lcd cycle
+  while (upState == HIGH && downState == HIGH){
+    
+    // clear 7-seg
+    void clearDisplay();
+    delay(100);
+    
+    //print 7-seg time
     char datestring[20];
-    String meridiem = " am"; 
     snprintf_P(datestring, 
             countof(datestring),
             PSTR("%02u/%02u/%04u %02u:%02u:%02u"),
@@ -212,9 +221,117 @@ void printDateTime(const RtcDateTime& dt)
             varHr = dt.Hour(),
             varMin = dt.Minute(),
             varSec = dt.Second() );
-    Serial.print(datestring);
-    Serial.print(meridiem);
-    Serial.print(" ");
+    hours = dt.Hour();
+    hour_dig1 = (hours/10U)%10;
+    hour_dig2 = (hours/1U)%10;
+    delay(200);
+  }
+
+     if (downState == LOW){
+        Serial.println("hour down button pressed");
+        varHr = dt.Hour();
+        varMin = dt.Minute();
+        varSec = dt.Second();
+        vardd = dt.Day();
+        varmm = dt.Month();
+        varyy = dt.Year();
+        varHr--;   //subtract one hour from the current time
+        RtcDateTime dateTime(varyy, varmm, vardd, varHr, varMin, varSec);
+        Rtc.SetDateTime(dateTime);
+    }
+    if (upState == LOW){
+      Serial.println("hour up button pressed");
+      varHr = dt.Hour();
+      varMin = dt.Minute();
+      varSec = dt.Second();
+      vardd = dt.Day();
+      varmm = dt.Month();
+      varyy = dt.Year();
+      varHr++;   //add one hour too the current time
+      RtcDateTime dateTime(varyy, varmm, vardd, varHr, varMin, varSec);
+      Rtc.SetDateTime(dateTime);
+    }
+    //check if alarm button was pressed
+    if (alarmState == LOW /*&& alarm is off*/){
+    //if pressed blink minutes
+      while (upState == HIGH && downState == HIGH){
+        lcd.setCursor(0,0);
+        lcd.print("                ");
+        delay(100)
+        char datestring[20];
+        snprintf_P(datestring, 
+            countof(datestring),
+            PSTR("%02u/%02u/%04u %02u:%02u:%02u"),
+            varmm = dt.Month(),
+            vardd = dt.Day(),
+            varyy = dt.Year(),
+            varHr = dt.Hour(),
+            varMin = dt.Minute(),
+            varSec = dt.Second() );
+            Minutes = varMin; 
+        min_dig1 = (Minutes/10U)%10;
+        min_dig2 = (Minutes/1U)%10;
+     
+        lcd.setCursor(5,0);
+        for (int i = 1; i <= min_dig1; i++){
+          lcd.print(char(255)); 
+        }
+        lcd.setCursor(4,1); 
+            for (int i = 1; i <= min_dig2; i++){
+          lcd.print(char(255)); 
+        } 
+       delay(200);
+      }
+     if (downState == LOW){
+        Serial.println("minute down button pressed");
+        varHr = dt.Hour();
+        varMin = dt.Minute();
+        varSec = dt.Second();
+        vardd = dt.Day();
+        varmm = dt.Month();
+        varyy = dt.Year();
+        varMin--;   //subtract one minute from the current time
+        RtcDateTime dateTime(varyy, varmm, vardd, varHr, varMin, varSec);
+        Rtc.SetDateTime(dateTime); 
+    }
+    if (upState == LOW){
+        Serial.println("minute up button pressed");
+        varHr = dt.Hour();
+        varMin = dt.Minute();
+        varSec = dt.Second();
+        vardd = dt.Day();
+        varmm = dt.Month();
+        varyy = dt.Year();
+        varMin++;   //add one minute to the current time
+        RtcDateTime dateTime(varyy, varmm, vardd, varHr, varMin, varSec);
+        Rtc.SetDateTime(dateTime); 
+    }
+}
+}
+/*void setAlarm(){
+    lcd.setCursor(0,2)
+    lcd.print(" */
+    // call printDateTime(12:00)
+    // call a similar function to checkButtons to allow the time to move up or time. Difference: you're not setting the RTC, you're just temporarily displaying
+    // check if select button is released
+    // save the time selection
+    
+    return time_min, time_hrs;
+}
+//print RTC time on console, LCD, and 7-seg
+void printDateTime(const RtcDateTime& dt)
+{ 
+    char datestring[20];
+    snprintf_P(datestring, 
+            countof(datestring),
+            PSTR("%02u/%02u/%04u %02u:%02u:%02u"),
+            varmm = dt.Month(),
+            vardd = dt.Day(),
+            varyy = dt.Year(),
+            varHr = dt.Hour(),
+            varMin = dt.Minute(),
+            varSec = dt.Second() );
+    Serial.println(datestring);
     
 //print minutes on LCD
     lcd.clear();         
@@ -230,7 +347,6 @@ void printDateTime(const RtcDateTime& dt)
         for (int i = 1; i <= min_dig2; i++){
       lcd.print(char(255)); 
     } 
-    delay(0);
     
 //print hours on 7-seg
 
