85738ed34469f9b66a46cbf9913ad7b07cb3cd83
hps-annah <asingh@hps-program.com>
2022-07-31 13:21:28 -0400

    Update buttons_test.ino

--- buttons_test/buttons_test.ino
+++ buttons_test/buttons_test.ino
@@ -3,38 +3,39 @@ const int downButton = 3;
 const int upButton = 4;
 const int setButton = 5;
 const int alarmButton = 12;
-int downState = 0;
-int upState = 0;
-int setState = 0;
-int alarmState = 0;
 
 void setup() {
+  
   Serial.begin(57600);
+  Serial.println();
+
   //setup buttons
   pinMode(downButton, INPUT);
   pinMode(upButton, INPUT);
   pinMode(alarmButton, INPUT);
-  pinMode(setButton, INPUT);   
+  pinMode(setButton, INPUT);  
 }
 
 void loop() {
-  //read the states of each button
-  downState == digitalRead(downButton);
-  upState == digitalRead(upButton);
-  alarmState == digitalRead(alarmButton);
-  setState == digitalRead(setButton);
+//read the states of each button
+  int downState = digitalRead(downButton);
+  int upState = digitalRead(upButton);
+  int alarmState = digitalRead(alarmButton);
+  int setState = digitalRead(setButton);
 
-  
+//serial print if button is pressed
   if (downState == LOW){
     Serial.println("down button pressed");
   }
   if (upState == LOW){
     Serial.println("up button pressed");
   }
+  */
   if (alarmState == LOW){
     Serial.println("alarm button pressed");
   }
   if (setState == LOW){
     Serial.println("set button pressed");
   }
+  
 }
